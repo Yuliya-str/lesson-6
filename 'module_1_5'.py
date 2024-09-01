9d20893c1803e94c1dced4509aa69ceb58377b96
Yuliya-str <gliceriya@yandex.ru>
2024-09-01 22:29:22 +0300

    Add files via upload

---
+++ 'module_1_5'.py
@@ -0,0 +1,7 @@
+immutable_var=1,2,3,2.4,'sun'
+print(type (immutable_var))
+print(immutable_var)
+mutable_list=["sun","sky","clouds"]
+print(mutable_list)
+mutable_list[1]='rain'
+print(mutable_list)
